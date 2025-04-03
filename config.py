d8e0b94f8c5cf3cf1ed3af771acc3aa911a46d84
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-04-04 01:05:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "f553398ca957b9c92bcb672b05557038")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1423807625').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1423807625 6520490787').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "file_sharing_bot03_bot") # without @
 PORT = environ.get("PORT", "8080")
 
